ff76c28e692646140b192f7633f8204276903d13
psren <paddy.heller@gmail.com>
2012-05-14 14:08:16 +0300

    Update fuel/app/config/development/db.php

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -5,18 +5,13 @@
 
 return array(
 	'default' => array(
-		/*'connection'  => array(
-			'dsn'        => 'mysql:host=localhost:3306;dbname=mailer',
-			'username'   => 'root',
-			'password'   => '7v48dz',
-		),*/
 		'type'           => 'mysqli',
     	'connection'     => array(
 	        'hostname'       => 'localhost',
 	        'port'           => '3306',
 	        'database'       => 'mailer',
 	        'username'       => 'root',
-	        'password'       => '7v48dz',
+	        'password'       => '',
 	        'persistent'     => false,
 	    ),
 	),
